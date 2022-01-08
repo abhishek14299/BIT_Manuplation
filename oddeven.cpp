8841460161eda41001972214f1eeb4922c240840
utkarshtyagi2 <tyagiutkarsh16@gmail.com>
2022-01-08 14:03:00 +0530

    swap odd bits with even bits

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+#define ODDEVEN(a) (((a&0x55555555)<<1) | ((a&0xAAAAAAAA)>>1))
+
+int main(){
+    unsigned int a=1;
+    /* 00000000,00000000,00000000,00001101
+                                        */
+
+
+   unsigned int swap= ODDEVEN(a);
+
+    std::cout<<swap<<std::endl;
+    
+    return 0;
+
+}
